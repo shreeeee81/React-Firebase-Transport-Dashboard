8610fafb734f8024a1eb88f463b5f5887d2f2978
SHREEHARI B V <42206480+shreeeee81@users.noreply.github.com>
2021-06-22 14:09:33 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDxcjyWl7fDb-VQi-w8bmNmtaG5BMjX1rM",
-  authDomain: "smart-image-transport.firebaseapp.com",
-  databaseURL: "https://smart-image-transport-default-rtdb.firebaseio.com",
-  projectId: "smart-image-transport",
-  storageBucket: "smart-image-transport.appspot.com",
-  messagingSenderId: "228962334590",
-  appId: "1:228962334590:web:b8d3749deb54f07145a52f",
-  measurementId: "G-945ZSVXBKQ"
+    apiKey: "......Your API Key.......",
+  authDomain: "...........",
+  databaseURL: ".........",
+  projectId: "............",
+  storageBucket: "...........",
+  messagingSenderId: "...........",
+  appId: "...........",
+  measurementId: "..........."
 
   
 };
